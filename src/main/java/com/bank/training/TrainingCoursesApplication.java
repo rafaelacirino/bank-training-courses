86a5cd22f195250ef87dcd4d03ef4a0e67114f50
rafaelacirino <rafaelaborbaf@gmail.com>
2025-11-27 10:20:21 +0100

    add unversioned files

---
+++ src/main/java/com/bank/training/TrainingCoursesApplication.java
@@ -0,0 +1,13 @@
+package com.bank.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingCoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingCoursesApplication.class, args);
+	}
+
+}
